11a387eaef1c3bfb02b03155b46f99d1250e62f6
Hristiyan Teofanov Stefanov <dogman19991@gmail.com>
2020-10-19 19:32:06 +0300

    initial commit

---
+++ RoomFindSolution/RoomFind/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RoomFind.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
